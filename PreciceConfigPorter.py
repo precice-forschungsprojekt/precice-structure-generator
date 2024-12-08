8e43ab157c376467a40ff5cfb5aef38921bb100c
Toddelismyname <thore.schweikert@outlook.com>
2024-12-08 20:34:32 +0100

    add general xml transform operation and sub operations

--- PreciceConfigPorter.py
+++ PreciceConfigPorter.py
@@ -114,3 +114,103 @@ def create_key_value_pair(attribute_str):
     key = re.sub(r'[^a-zA-Z0-9]', '', key)
     value = re.sub(r'[^a-zA-Z0-9]', '', value)
     return key, value
+
+def xml_transform(line: str, 
+                 input_tag: str = None, 
+                 output_tag: str = None, 
+                 remove_attrs: list = None, 
+                 add_attrs: dict = None, 
+                 replace_attrs: dict = None, 
+                 logger=None):
+    """
+    Perform comprehensive XML line transformations
+    
+    Args:
+        line (str): Input XML line to transform
+        input_tag (str, optional): Original tag name to replace
+        output_tag (str, optional): New tag name
+        remove_attrs (list, optional): List of attributes to remove
+        add_attrs (dict, optional): Dictionary of attributes to add
+        replace_attrs (dict, optional): Dictionary of attributes to replace
+        logger (optional): Logger for tracking transformations
+    
+    Returns:
+        str: Transformed XML line
+    """
+    # If line is empty or doesn't contain input tag, return original line
+    if not line or (input_tag and input_tag not in line):
+        return line
+    
+    # Extract indentation
+    indentation = line.split('<')[0]
+    
+    # Parse current attributes
+    attributes = get_attributes(line)
+    
+    # Rename tag if specified
+    if input_tag and output_tag:
+        line = line.replace(input_tag, output_tag)
+    
+    # Remove specified attributes
+    if remove_attrs:
+        for attr in remove_attrs:
+            line = re.sub(f'{attr}="[^"]*"', '', line)
+    
+    # Replace attributes
+    if replace_attrs:
+        for old_attr, new_attr in replace_attrs.items():
+            line = re.sub(f'{old_attr}="([^"]*)"', f'{new_attr}="\\1"', line)
+    
+    # Add new attributes
+    if add_attrs:
+        # Remove trailing />
+        line = line.rstrip('/>') + ' '
+        # Add new attributes
+        for attr, value in add_attrs.items():
+            line += f'{attr}="{value}" '
+        # Close tag
+        line = line.rstrip() + '/>'
+    
+    # Clean up whitespace
+    line = re.sub(r'\s+>', '>', line)
+    line = re.sub(r'\s{2,}', ' ', line)
+    
+    # Reattach indentation
+    line = indentation + line.lstrip()
+    
+    if logger:
+        logger.info(f"XML Transform: {line}")
+    
+    return line
+
+def rename_tag(line: str, input_tag: str, output_tag: str, logger=None):
+    """Rename an XML tag"""
+    return xml_transform(line, input_tag=input_tag, output_tag=output_tag, logger=logger)
+
+def remove_attribute(line: str, attr: str, logger=None):
+    """Remove a specific attribute from an XML tag"""
+    return xml_transform(line, remove_attrs=[attr], logger=logger)
+
+def add_attribute(line: str, attr: str, value: str, logger=None):
+    """Add a new attribute to an XML tag"""
+    return xml_transform(line, add_attrs={attr: value}, logger=logger)
+
+def replace_attribute(line: str, old_attr: str, new_attr: str, logger=None):
+    """Replace an attribute name while preserving its value"""
+    return xml_transform(line, replace_attrs={old_attr: new_attr}, logger=logger)
+
+def rename_tag_and_remove_attribute(line: str, input_tag: str, output_tag: str, attr: str, logger=None):
+    """Rename a tag and remove a specific attribute"""
+    return xml_transform(line, 
+                         input_tag=input_tag, 
+                         output_tag=output_tag, 
+                         remove_attrs=[attr], 
+                         logger=logger)
+
+def rename_tag_and_replace_attribute(line: str, input_tag: str, output_tag: str, old_attr: str, new_attr: str, logger=None):
+    """Rename a tag and replace an attribute"""
+    return xml_transform(line, 
+                         input_tag=input_tag, 
+                         output_tag=output_tag, 
+                         replace_attrs={old_attr: new_attr}, 
+                         logger=logger)
